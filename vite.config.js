81ab334add42d8b94dc6b7a8041b24bb6a53488f
Gameel-ali1 <Gamel_ali@outlook.com>
2025-07-16 18:11:30 +0300

    ne

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env.NODE_ENV': '"production"'
+  build: {
+    minify: false, // Disable minification temporarily
+    rollupOptions: {
+      output: {
+        format: 'es'
+      }
+    }
   }
-});
+})
