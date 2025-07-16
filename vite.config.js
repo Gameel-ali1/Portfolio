f6e73f3c2102b9e89e830e88b93f665ee9f2d8e3
Gameel-ali1 <Gamel_ali@outlook.com>
2025-07-16 17:51:55 +0300

    fixed deployment issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -11,8 +10,14 @@ export default defineConfig({
     }
   },
   build: {
-    commonjsOptions: {
-      transformMixedEsModules: true
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
     }
+  },
+  define: {
+    global: 'globalThis'
   }
-}) 
+})
