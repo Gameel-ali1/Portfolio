3b35cbcd8d81b0141df6fd03f05298efb63cfe6f
Gameel-ali1 <Gamel_ali@outlook.com>
2025-07-16 18:02:07 +0300

    second fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'automatic'
+  })],
   resolve: {
     alias: {
       'react': 'react',
@@ -12,12 +14,10 @@ export default defineConfig({
   build: {
     target: 'esnext',
     rollupOptions: {
+      external: [],
       output: {
-        manualChunks: undefined
+        format: 'es'
       }
     }
-  },
-  define: {
-    global: 'globalThis'
   }
 })
